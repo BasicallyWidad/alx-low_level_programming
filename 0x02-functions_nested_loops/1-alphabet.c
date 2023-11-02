be5f4d7090069e72524bda40248b5263e811c862
BasicallyWidad <widadamier123@gmail.com>
2023-11-02 12:27:03 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_alphabit - prints the alphabit in lower case followed by a new line
+ * Return: 0
+ */
+
+void print_alphabit(void){
+	char letter;
+	for(letter=a,letter<=z,letter++)
+		{_putchar(letter);}
+	_putchar('\n');
+	return(0);}
